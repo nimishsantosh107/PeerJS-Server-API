589094e0dd7bad76cbf1cf4bae9348b051b3ce88
Nimish Santosh <nimishsantosh107@icloud.com>
2019-09-01 20:00:11 +0530

    peers express + cors

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: process.env.PORT || 9000 , path: '/vr'});
+const express = require('express');
+const cors = require('cors');
+const ExpressPeerServer = require('peer').ExpressPeerServer;
+
+var PORT = process.env.PORT || 9000;
+
+var app = express();
+app.use(cors());
+
+app.get('/test', function(req, res, next) { res.send('SERVER UP'); });
+
+var server = app.listen(PORT);
+ 
+var options = {
+   		debug: true
+	}
+var peerserver = ExpressPeerServer(server, options);
+app.use('/', peerserver);
