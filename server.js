ec524280f581bb34a80072e100c62d81f2e32625
mohanpierce99 <itsmohanpierce@gmail.com>
2019-09-12 20:40:49 +0530

    onlinestatusfix

--- server.js
+++ server.js
@@ -12,31 +12,57 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
-const socketIO = require('socket.io'); 
+const socketIO = require('socket.io');
+const axios = require('axios');
+var firebase = require('firebase');
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDxUdsBOiWl41ASEHweGZdhdCZtXDvPOg8",
+    authDomain: "vr-theatre.firebaseapp.com",
+    databaseURL: "https://vr-theatre.firebaseio.com",
+    projectId: "vr-theatre",
+    storageBucket: "",
+    messagingSenderId: "69732209271",
+    appId: "1:69732209271:web:72b6bf3a671d3b8f43ec72"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db=firebase.firestore();
+
 const ExpressPeerServer = require('peer').ExpressPeerServer;
 
 var PORT = process.env.PORT || 9000;
 
 var app = express();
 var server = http.Server(app);
-var io = socketIO(server); 
+var io = socketIO(server);
 app.use(cors());
 
 //SEE IF SITE IS RUNNING
-app.get('/status', function(req, res) { res.send('SERVER UP'); });
-app.get('/privacypolicy', function (req, res,) { res.sendFile(path.join(__dirname,'privacypolicy.html')); });
+app.get('/status', function (req, res) {
+	res.send('SERVER UP');
+});
+app.get('/privacypolicy', function (req, res, ) {
+	res.sendFile(path.join(__dirname, 'privacypolicy.html'));
+});
 
 //LAUNCH
-server.listen(PORT, ()=>{console.log(`SERVER UP ON PORT: ${PORT}`)});
+server.listen(PORT, () => {
+	console.log(`SERVER UP ON PORT: ${PORT}`)
+});
 
 //CONFIG PEER-JS SERVER
-var peerserver = ExpressPeerServer(server, {debug: true});
+var peerserver = ExpressPeerServer(server, {
+	debug: true
+});
 app.use('/peer', peerserver);
 
 
 //SIGNALLING AND CANDIDATE INFO
 io.on("connection", (socket) => {
-
 	//ROOM
 	socket.on("joinRoom", async (data) => {
 		socket.room = data;
@@ -61,6 +87,13 @@ io.on("connection", (socket) => {
 		console.log(`~~ SOCKETID: ${socket.id} | PEERID: ${socket.peerid}`)
 	});
 
+	socket.on('onlineStatus', (uid) => {
+		console.log(uid + " online ");
+		socket.uid=uid;
+        db.collection("users").doc(uid).update({onlineStatus:true}).then(()=>{
+			socket.emit("status_set",true);
+		});
+	});
 
 	socket.on("controls", (obj) => {
 		console.log("Server:controls");
@@ -83,6 +116,9 @@ io.on("connection", (socket) => {
 
 	//HANDLE DISCONNECTION
 	socket.on("disconnect", async () => {
+	
+		db.collection("users").doc(socket.uid).update({onlineStatus:false});
+		
 		if (socket.room && io.sockets.adapter.rooms[socket.room]) {
 			socket.broadcast.to(socket.room).emit('newLeaving', {
 				leftPeerid: socket.peerid
